d03bc60ed7c21f791e0b160da869ce8ed835b869
Prayas Sinha <ralphraout@gmail.com>
2024-11-08 21:42:07 +0530

    Fixed Button Colour

--- app.js
+++ app.js
@@ -130,16 +130,16 @@ function drawButton(index)
     const canv = document.getElementById("canvas");
     const ctx = canv.getContext("2d");
 
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "Black";
     ctx.fillRect(scenes[index].choiceBoxes.a.x1,scenes[index].choiceBoxes.a.y1,scenes[index].choiceBoxes.a.x2-scenes[index].choiceBoxes.a.x1,scenes[index].choiceBoxes.a.y2-scenes[index].choiceBoxes.a.y1);
     
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "White";
     ctx.fillText(scenes[index].choiceTexts.a,scenes[index].choiceBoxes.a.x1,scenes[index].choiceBoxes.a.y1);
     
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "Black";
     ctx.fillRect(scenes[index].choiceBoxes.b.x1,scenes[index].choiceBoxes.b.y1,scenes[index].choiceBoxes.b.x2-scenes[index].choiceBoxes.b.x1,scenes[index].choiceBoxes.b.y2-scenes[index].choiceBoxes.b.y1);
 
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "White";
     ctx.fillText(scenes[index].choiceTexts.b,scenes[index].choiceBoxes.b.x1,scenes[index].choiceBoxes.b.y1);
     
 }
