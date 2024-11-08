5387515b11896044e59b30d1adae3168d28f358e
Prayas Sinha <ralphraout@gmail.com>
2024-11-08 06:01:51 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,16 +1,16 @@
 //import scenes from "./scenes.json" with { type: "json" };
-export const scenes = [
-    {
+export const scenes =
+    [{
         "sceneIndex":"0",
         "sceneBgPath":"./assets/bg0.png",
         "choiceBoxes":{"a":{"x1":"120","y1":"140","x2":"120","y2":"140"},"b":{"x1":"120","y1":"140","x2":"120","y2":"140"}},
         "choiceTexts":{"a":"Brush","b":"Don't Brush"},
         "textPrompt":"You just woke up prettyface, wanna go brush your teeth eh?"
-    }
-];
-export const scene = JSON.parse(scenes);
+    }];
+
 export function loadScene(index)
 {
+    let scene = JSON.parse(scenes[index]);
     drawBgScene(scene[index].sceneBgPath);
     drawTextBox(scene[index].textPrompt + "           " +scene[index].choiceTexts.a + " , "+scene[index].choiceTexts.b);
 
