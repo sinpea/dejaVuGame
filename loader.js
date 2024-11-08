057a2657348295bc3c370776688aec528a204adb
Prayas Sinha <ralphraout@gmail.com>
2024-11-08 06:20:41 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,24 +1,24 @@
 //import scenes from "./scenes.json" with { type: "json" };
 const scenes =
     [{
-        "sceneIndex":"0",
-        "sceneBgPath":"./assets/bg0.png",
-        "choiceBoxes":{"a":{"x1":"120","y1":"140","x2":"120","y2":"140"},"b":{"x1":"120","y1":"140","x2":"120","y2":"140"}},
-        "choiceTexts":{"a":"Brush","b":"Don't Brush"},
-        "textPrompt":"You just woke up prettyface, wanna go brush your teeth eh?"
+        sceneIndex:"0",
+        sceneBgPath:"./assets/bg0.png",
+        choiceBoxes:{a:{x1:"120",y1:"140",x2:"120",y2:"140"},b:{x1:"120",y1:"140",x2:"120",y2:"140"}},
+        choiceTexts:{a:"Brush",b:"Don't Brush"},
+        textPrompt:"You just woke up prettyface, wanna go brush your teeth eh?"
     }];
 
 function loadScene(index)
 {
-    let scene = JSON.parse(scenes[index]);
-    drawBgScene(scene[index].sceneBgPath);
-    drawTextBox(scene[index].textPrompt + "           " +scene[index].choiceTexts.a + " , "+scene[index].choiceTexts.b);
+    //let scene = JSON.parse(scenes[index]);
+    drawBgScene(scenes[index].sceneBgPath);
+    drawTextBox(scenes[index].textPrompt + "           " +scenes[index].choiceTexts.a + " , "+scenes[index].choiceTexts.b);
 
-    choiceCoordinateArr[0].corner1.x = scene[index].choiceBoxes.a.x1;
-    choiceCoordinateArr[1].corner2.x = scene[index].choiceBoxes.b.x2;
+    choiceCoordinateArr[0].corner1.x = scenes[index].choiceBoxes.a.x1;
+    choiceCoordinateArr[1].corner2.x = scenes[index].choiceBoxes.b.x2;
 
-    choiceCoordinateArr[0].corner1.y = scene[index].choiceBoxes.a.y1;
-    choiceCoordinateArr[1].corner2.y = scene[index].choiceBoxes.b.y2;
+    choiceCoordinateArr[0].corner1.y = scenes[index].choiceBoxes.a.y1;
+    choiceCoordinateArr[1].corner2.y = scenes[index].choiceBoxes.b.y2;
 
     SceneIndex = index;
 }
